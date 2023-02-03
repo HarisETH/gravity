2d1818aaff174d1d823b4e89338bc61bcd9a2558
Haris <hariskumar.eth@gmail.com>
2023-02-03 07:05:32 +0530

    fixed line-wrap and caret bugs

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -4,30 +4,27 @@ import { javascript } from "@codemirror/lang-javascript";
 import { githubDark } from "@uiw/codemirror-themes-all";
 import { dracula } from "@uiw/codemirror-themes-all";
 import download from "downloadjs";
-import * as htmlToImage from 'html-to-image';
-import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
-
-
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
+import { EditorView } from "codemirror";
 
 const Editor = () => {
   const exportContent = useRef();
   const [lineStatus, setLineStatus] = useState(false);
 
-  const [exportComponent, setExComp] = useState()
+  const [exportComponent, setExComp] = useState();
 
-  const handleClick = () =>{
-    console.log(exportComponent)
-    htmlToImage.toPng(exportComponent)
-    .then(function(dataUrl){
-      download(dataUrl, 'reffer.png')
-    })
-  } 
+  const handleClick = () => {
+    console.log(exportComponent);
+    htmlToImage.toPng(exportComponent).then(function (dataUrl) {
+      download(dataUrl, "reffer.png");
+    });
+  };
 
   useEffect(() => {
     const editorExportClass = document.getElementsByClassName("cm-editor");
-    let exportComp = document.getElementById('capture');
-  setExComp(exportComp)
-
+    let exportComp = document.getElementById("capture");
+    setExComp(exportComp);
 
     // console.log(editorExportClass);
     setEditorClass(editorExportClass[0]);
@@ -37,10 +34,8 @@ const Editor = () => {
       console.log(el);
 
       if (lineStatus) {
-        
         el.classList.add("lineActive");
       } else {
-        
         el.classList.remove("lineActive");
       }
     });
@@ -48,31 +43,34 @@ const Editor = () => {
 
   const [editorTheme, setEditorTheme] = useState(githubDark);
   const [editorClass, setEditorClass] = useState();
-console.log(editorClass)
+  console.log(editorClass);
   return (
     <section className="w-full px-4 flex flex-col gap-4">
       <div className="toolbar bg-gray-900 p-2 text-white rounded-md flex gap-10">
         <button onClick={() => setLineStatus(!lineStatus)}>Lines</button>
         <button onClick={() => setEditorTheme(dracula)}>Dark theme</button>
-        <button onClick={() => setEditorTheme('light')}>Light theme</button>
+        <button onClick={() => setEditorTheme("light")}>Light theme</button>
         <button onClick={() => setEditorTheme(githubDark)}>Default</button>
-
-        
-
-
       </div>
-      <div className=" border-black resize rounded-md w-min p-10 bg-black" id="capture" ref={exportContent}>
+      <div
+        className=" border-black resize rounded-md w-min p-10 gradient-cover-one max-w-4xl"
+        id="capture"
+        ref={exportContent}
+      >
         <CodeMirror
-        
-          className="editor-box caret-transparent outline-none select-none pointer-events-none"
+          className="editor-box p-10  outline-none select-none min-w-ful w-max max-w-xl"
           value="console.log('hello world!');"
-          extensions={[javascript({ jsx: true })]}
+          extensions={
+            [javascript({ jsx: true }),
+              EditorView.lineWrapping
+            
+            ]
+            
+          }
           theme={editorTheme}
+          lineWrapping={true}
         />
       </div>
-      
-
-      
 
       <button onClick={handleClick}>Download</button>
     </section>
